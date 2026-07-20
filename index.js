78946e706fa9eb7f3f0a12aacbc59e78696117d1
junaidthodge <junaidthodge665@gmail.com>
2026-07-20 11:44:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('maharashtra transport');
+    res.send('junaid transport');
 });
 
 app.listen(process.env.PORT || 3000);
