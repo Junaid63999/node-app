ac0f1e958e669bf4b90540d1c48b8ef70b295d9a
junaid63999 <junaidthodge665@gmail.com>
2026-07-20 07:24:57 +0000

    ydb

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('mahebub from hadolti);
+    res.send('mahebub from hadolti');
 });
 
 app.listen(process.env.PORT || 3000);
