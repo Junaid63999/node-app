cf29d8b5eb56c1c41a4d32dc094b78b3f663a3ed
junaid63999 <junaidthodge665@gmail.com>
2026-07-20 09:52:03 +0000

    all

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('mahebub from hadolti');
+    res.send('junaid also  from a hadolti');
 });
 
 app.listen(process.env.PORT || 3000);
